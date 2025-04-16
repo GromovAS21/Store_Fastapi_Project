a4e6cebcc52528a35615c407a996b4fef66c0df6
Anton Gromov <seed14112@yandex.ru>
2025-04-16 09:33:50 +0300

    [+] Добавление версионирования

--- main.py
+++ main.py
@@ -4,9 +4,16 @@ from routers import auth, permissions, reviews
 from routers import products, categories
 
 app = FastAPI()
+app_v1 = FastAPI(
+    title="My API v1",
+    description="The first version of my API",
+)
 
-app.include_router(categories.router)
-app.include_router(products.router)
-app.include_router(auth.router)
-app.include_router(permissions.router)
-app.include_router(reviews.router)
+app_v1.include_router(categories.router)
+app_v1.include_router(products.router)
+app_v1.include_router(auth.router)
+app_v1.include_router(permissions.router)
+app_v1.include_router(reviews.router)
+
+
+app.mount("/v1", app_v1) # Версионирование
